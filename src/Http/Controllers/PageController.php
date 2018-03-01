99f0bd5e302f9a75da7ebf0c023ace78b56610aa
Jean-Pierre Gassin <jeanpierre.gassin@gmail.com>
2018-03-01 12:45:21 +1000

    Add breadcrumbs to new page block rendering

--- src/Http/Controllers/PageController.php
+++ src/Http/Controllers/PageController.php
@@ -3,11 +3,36 @@
 namespace Pvtl\VoyagerPageBlocks\Http\Controllers;
 
 use Pvtl\VoyagerPageBlocks\Page;
+use Pvtl\VoyagerFrontend\Http\Controllers\VoyagerFrontendController;
+use Illuminate\Http\Request;
 use Illuminate\Routing\Controller;
 use Illuminate\Support\Facades\View;
 
 class PageController extends Controller
 {
+    protected $request;
+
+    protected $frontendController;
+
+    /**
+     * PageController constructor.
+     *
+     * @param Request $request
+     * @param VoyagerFrontendController $frontendController
+     */
+    public function __construct(Request $request, VoyagerFrontendController $frontendController)
+    {
+        $this->request = $request;
+        $this->frontendController = $frontendController;
+    }
+
+    /**
+     * Fetch all pages and their associated blocks
+     *
+     * @param string $slug
+     *
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
     public function getPage($slug = 'home')
     {
         $page = Page::where('slug', '=', $slug)->firstOrFail();
@@ -23,13 +48,21 @@ class PageController extends Controller
             });
         
         // Override standard body content, with page block content
-        $page['body'] = view('voyager-page-blocks::default', compact('page', 'blocks'));
+        $page['body'] = view('voyager-page-blocks::default', [
+            'page' => $page,
+            'blocks' => $blocks,
+            'breadcrumbs' => $this->frontendController->getBreadcrumbs($this->request),
+        ]);
 
         // Check that the page Layout View exists
         $layout = (!empty($page->layout)) ? $page->layout : 'default';
         if (!View::exists('voyager-frontend::layouts.' . $layout)) $layout = 'default';
 
         // Return the full page
-        return view('voyager-frontend::modules/pages/default', compact('page', 'layout'));
+        return view('voyager-frontend::modules/pages/default', [
+            'page' => $page,
+            'layout' => $layout,
+            'breadcrumbs' => $this->frontendController->getBreadcrumbs($this->request),
+        ]);
     }
 }
