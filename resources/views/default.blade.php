e571bd3dbb1535ca2f2a9ea6fde1f0bf3392322f
Matt Mcnamee <matt@mcnamee.co>
2018-03-05 14:32:03 +1000

    Echo the HTML content instead of escaped HTML

--- resources/views/default.blade.php
+++ resources/views/default.blade.php
@@ -1,11 +1,9 @@
 @foreach($blocks as $block)
     @if (!empty($block->html))
-        {{ $block->html }}
-    @elseif(View::exists('voyager-page-blocks::block_templates/' . $block->template))
+        @php echo (string)$block->html @endphp
+    @elseif (View::exists('voyager-page-blocks::block_templates/' . $block->template))
         @component('voyager-page-blocks::block_templates/' . $block->template, ['blockData' => $block->data])
         @endcomponent
-    @elseif($block->template === 'include' && View::exists('voyager-page-blocks-includes::' . $block->path))
-        @component('voyager-page-blocks-includes::' . $block->path, ['blockData' => $block->data]) @endcomponent
     @else
         <div class="page-block">
             <div class="callout alert">
