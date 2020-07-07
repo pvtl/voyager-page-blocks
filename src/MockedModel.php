a5340cfe208a67d79d8c4519d91d81d34945d055
Liamso <liam.a.southwell@gmail.com>
2020-07-07 13:16:49 +1000

    Handle no-content blocks - Fixes #32

--- src/MockedModel.php
+++ src/MockedModel.php
@@ -7,13 +7,15 @@ class MockedModel
 {
     public function __construct($datem)
     {
-        foreach ($datem as $key => $data) {
-            $this->$key = $data;
+        if (is_array($datem)) {
+            foreach ($datem as $key => $data) {
+                $this->$key = $data;
+            }
         }
     }
 
     public function getKey()
     {
-        return $this->id;
+        return $this->id ? $this->id : '';
     }
 }
